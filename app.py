b19ca08c880ec54b4c41b124623586fa046ab716
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-26 19:16:47 -0500

    all routes functional

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from flask import (render_template, url_for, request, redirect)
 import datetime
 
 
-"""
-Create the Homepage route/view
-Route: /
-
-This view should render a page of all of the projects, where each project displays the following fields:
-
-
-Create an interface for a portfolio web application. The main (index) page lists your projects including the project title and short description. Each project links to a detail page that displays the title, date, and description.
-"""
-
-
 @app.route('/')
 def index():
     projects = Project.query.all()
@@ -24,21 +13,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-"""
-Create the Detail route/view
-Route: /project/<id>
-
-This view should render a detail page of a project, it should display the following fields on the page:
-
-Title
-Date
-Description
-Skills practiced
-Link to the project’s GitHub repo
-NOTE: This page should contain a link/button that takes the user to the Edit route for the project with this <id>.
-"""
 
 
 @app.route('/project/<id>')
@@ -62,33 +36,21 @@ def add_project():
     return render_template('projectform.html')
 
 
-"""
-Create the Edit route/view
-Route: /project/<id>/edit
-
-Create an edit view with the route /project/<id>/edit that allows the user to edit the project with an id of the <id> passed in:
-
-Title
-Date
-Description
-Skills practiced
-GitHub repo link
-Ideally, you should pre-populate each form field with the existing data on load. So the form is filled out with the existing data so the User can easily see what the value is and make edits to the form to make the update.
-
-NOTE: Updating a project should not result in a new project being created, this behavior would not be seen as editing this would be adding a new project. To check this, you can simply make an edit and then reload the listing page to see if a duplicate record was created.
-"""
-
-
 @app.route('/project/<id>/edit', methods=['GET', 'POST'])
 def edit_project(id):
     project = Project.query.get_or_404(id)
+    if request.form:
+        project.title = request.form['title']
+        formatted_date = clean_date(request.form['date'])
+        if type(formatted_date) != datetime.datetime:
+            return render_template('editproject.html', project=project, date_error=True)
+        project.date = formatted_date
+        project.skills = request.form['skills']
+        project.repo_link = request.form['github']
+        project.description = request.form['desc']
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('editproject.html', project=project)
-
-
-"""
-Create the Delete route
-Create a delete route to delete the project from the database. When the delete button is clicked by the user, the project will be removed from the database and they will be redirected to the homepage.
-"""
 
 
 @app.route('/project/<id>/delete')
